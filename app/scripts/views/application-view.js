dcb5c1069a156c460edefd74c2ccb85b95039bee
Kyle Beikirch <kyle.beikich@gmail.com>
2012-11-12 12:13:06 -0800

    refactor for backbone

--- app/scripts/views/application-view.js
+++ app/scripts/views/application-view.js
@@ -1,11 +1,18 @@
 StacheTrack.Views.applicationView = Backbone.View.extend({
+  events: {
+    "click #converter": "dosomething",
+  },
+  initialize: function() {
+    
+    $('#mustacheMolder').fadeOut();
 
-});
-$('#mustacheMolder').fadeOut();
-$('#converter').click(function() {
+  },
+  dosomething: function() {
     $('#imageHolder').fadeOut(0);
     var canvasImage = App.convertCanvasToImage();
     $('#imageHolder').append(canvasImage);
     $('#imageHolder').fadeIn(500);
     $('#mustacheMolder').fadeIn(500);
-  });
+  }
+
+});
