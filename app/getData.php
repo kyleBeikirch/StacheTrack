f3bbc56a4594c71ec7b302ca17def4cc16d5980e
Kyle Beikirch <kyle.beikich@gmail.com>
2012-11-20 11:38:58 -0800

    Get data php

---
+++ app/getData.php
@@ -0,0 +1,14 @@
+<?
+$user="664746_kyle";
+$password="beckham2579";
+$database="664746_wordpress";
+mysql_connect('mysql50-58.wc1.dfw1.stabletransit.com',$user,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+$lookid = $_GET["id"]; 
+$result = mysql_query("SELECT *  FROM `mustache`  WHERE `id` = '$lookid'");
+$row = mysql_fetch_row($result);
+echo $row[1]; // the email value
+echo "$$";
+echo $row[2];
+mysql_close();
+?>
