9a2735ed210537f5049d667ca9ed462f86c69d84
Kyle Beikirch <kyle.beikirch@gmail.com>
2012-11-26 07:47:25 -0800

    Cap scaling out of bounds

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -85,7 +85,7 @@ var App = {
                 {
                     App.videoScale = (App.comp[i].height + w) * m;
                     App.videoCenterX = ((App.comp[i].x - w / 2) * m) + 15;
-                    App.videoCenterY = ((App.comp[i].y - w / 2) * m) + 75;
+                    App.videoCenterY = Math.min(((App.comp[i].y - w / 2) * m) + 75, 300);
                 }
             }
 
